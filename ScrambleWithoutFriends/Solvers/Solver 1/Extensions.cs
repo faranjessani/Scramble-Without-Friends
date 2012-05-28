1960737526ef31902044361600a32dda9f537f35
Faran Jessani <solusfaran@gmail.com>
2012-05-27 22:27:25 -0400

    missed file

---
+++ ScrambleWithoutFriends/Solvers/Solver 1/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScrambleWithoutFriends
+{
+    public static class Extensions
+    {
+        public static int ToNodeIndex(this char character)
+        {
+            return char.ToLower(character) - 'a';
+        }
+    }
+}
